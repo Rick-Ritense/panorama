76620e4d99f9cad1c4ce7cb3c9cdfc5f88f67552
Rick Veenstra <rick.veenstra@amsterdam.nl>
2025-01-08 14:13:42 +0100

    initial commit - Added spring boot

---
+++ src/main/kotlin/com/ritense/panorama/PanoramaApplication.kt
@@ -0,0 +1,11 @@
+package com.ritense.panorama
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PanoramaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PanoramaApplication>(*args)
+}
---
+++ src/test/kotlin/com/ritense/panorama/PanoramaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ritense.panorama
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PanoramaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
