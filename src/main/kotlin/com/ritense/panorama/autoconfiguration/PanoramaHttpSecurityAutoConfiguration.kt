39567a6518b68439507e218295b0c15c971850d1
Rick Veenstra <rick.veenstra@amsterdam.nl>
2025-01-16 15:53:05 +0100

    feat:
    - Removed the localhost:4200

--- src/main/kotlin/com/ritense/panorama/autoconfiguration/PanoramaHttpSecurityAutoConfiguration.kt
+++ src/main/kotlin/com/ritense/panorama/autoconfiguration/PanoramaHttpSecurityAutoConfiguration.kt
@@ -89,7 +89,7 @@ class PanoramaHttpSecurityAutoConfiguration {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:4200")
+        configuration.allowedOrigins = listOf("*")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
         configuration.allowedHeaders = listOf("Authorization", "Content-Type", "X-API-KEY")
         configuration.allowCredentials = true
